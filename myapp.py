3149611243ab475fb2dd898e36268997524f1cf4
bagas-I56 <banabda@gmail.com>
2021-02-09 22:36:11 +0700

    added webhook

--- myapp.py
+++ myapp.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 import firebaseController
 
 app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        resp = request.get_json()
+        return Response(resp, status=200)
+    else:
+        return render_template('home.html')
 
 
 @app.route("/")
